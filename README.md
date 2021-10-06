eff4cc0565fb719b9588b38d01b2d58eb9f45e08
ThiagoKNA <thiagocanabarro2000@hotmail.com>
2021-10-06 10:03:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Padaria-do-Ze
+ 
---
+++ javascript/produtos.js
@@ -0,0 +1,72 @@
+const produtos = [
+    {
+        id: 1,
+        image: 'imagens/calca-virada.png',
+        title: 'Calça Virada',
+        description: '',
+        value: 9.99
+    },
+    {
+        id: 2,
+        image: 'imagens/pão-de-queijo.jpg',
+        title: 'Pão de Queijo',
+        description: '',
+        value: 10.00
+    },
+    {
+        id: 3,
+        image: 'imagens/pastel-de-carne.jpg',
+        title: 'Pastel',
+        description: '',
+        value: 6.00
+    },
+    {
+        id: 4,
+        image: 'imagens/pao-frances.jpg',
+        title: 'Pão Francês',
+        description: '',
+        value: 10.00
+    },
+    {
+        id: 5,
+        image: 'imagens/rosquinhas.jpg',
+        title: 'Rosquinhas',
+        description: '',
+        value: 5.00
+    },
+    {
+        id: 6,
+        image: 'imagens/sonho.jpg',
+        title: 'Sonho',
+        description: '',
+        value: 7.00
+    },
+    {
+        id: 7,
+        image: 'imagens/rocombole.jpg',
+        title: 'rocombole',
+        description: '',
+        value: 15.00
+    },
+    {
+        id: 8,
+        image: 'imagens/cento.jpg',
+        title: 'cento de salgado',
+        description: '',
+        value: 45.00
+    },
+    {
+        id: 9,
+        image: 'imagens/croissant.jpg',
+        title: 'croissant',
+        description: '',
+        value: 8.00
+    },
+    {
+        id: 10,
+        image: 'imagens/bolo chocolate.jpg',
+        title: 'Bolo Chocolate',
+        description: '',
+        value: 5.00
+    },
+]
